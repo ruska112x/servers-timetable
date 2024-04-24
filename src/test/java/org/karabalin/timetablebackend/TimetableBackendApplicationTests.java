a6b92f1580285dcffcc83f3a649c59b71ccd0576
Ruslan Karabalin <karabalin112@gmail.com>
2024-04-24 21:16:56 +0600

    feat: start project

---
+++ src/test/java/org/karabalin/timetablebackend/TimetableBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.karabalin.timetablebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimetableBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
