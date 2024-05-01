e65ba4a5791b883388204bfa9f190bb272c56fb0
Ruslan Karabalin <karabalin112@gmail.com>
2024-05-01 20:42:00 +0600

    feat: add validation exception handler

--- src/main/java/org/karabalin/timetablebackend/core/handlers/GlobalExceptionHandler.java
+++ src/main/java/org/karabalin/timetablebackend/core/handlers/GlobalExceptionHandler.java
@@ -5,11 +5,21 @@ import org.karabalin.timetablebackend.core.exceptions.EditInDatabaseException;
 import org.karabalin.timetablebackend.core.exceptions.NotFoundInDatabaseException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<String> methodArgumentNotValidExceptionHandler(MethodArgumentNotValidException e) {
+        StringBuilder stringBuilder = new StringBuilder();
+        e.getBindingResult().getAllErrors().forEach(error -> {
+            stringBuilder.append(((FieldError) error).getField()).append(": ").append(error.getDefaultMessage()).append("\n");
+        });
+        return new ResponseEntity<>(stringBuilder.toString(), HttpStatus.BAD_REQUEST);
+    }
 
     @ExceptionHandler(NotFoundInDatabaseException.class)
     public ResponseEntity<String> notFoundInDatabaseExceptionHandler(NotFoundInDatabaseException e) {
